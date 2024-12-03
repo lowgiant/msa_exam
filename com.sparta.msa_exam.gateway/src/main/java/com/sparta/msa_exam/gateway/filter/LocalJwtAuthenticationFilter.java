aedd8e56720270b2bb4e0dc2ed39e38a6034ad2a
lowgiant <lowgiant@gmail.com>
2024-12-04 03:13:29 +0900

    feat: Add Spring cloud gateway for Api gateway
    - 게이트웨이
    - 패키지명 com.sparta.msa_exam.gateway
    - 포트 19091
    - 엑세스 토큰 검증, 인증 안되면 401
    - auth 서버 경우 로그인, 회원가입 열려있음

---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/filter/LocalJwtAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package com.sparta.msa_exam.gateway.filter;
+
+import java.nio.charset.StandardCharsets;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Slf4j
+@Component
+public class LocalJwtAuthenticationFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.equals("/auth/sign-in") || path.equals("/auth/sign-up")) {
+            return chain.filter(exchange);
+        }
+
+        String token = extractToken(exchange);
+        if (token == null || !validateToken(token, exchange)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token, ServerWebExchange exchange) {
+        try {
+            SecretKey key = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm());
+
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(key)
+                    .build().parseSignedClaims(token);
+            log.info("#####payload :: " + claimsJws.getPayload().toString());
+            Claims claims = claimsJws.getBody();
+            exchange.getRequest().mutate()
+                    .header("X-User-Id", claims.get("username").toString())
+                    .header("X-Role", claims.get("role").toString())
+                    .header("Authorization", token)
+                    .build();
+
+            return true;
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            return false;
+        }
+    }
+
+
+
+}
